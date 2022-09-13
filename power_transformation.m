94ef55842d80d18bda45fee1d6d63ed78d96f1ee
tirthkp <tirthp57@gmail.com>
2022-09-13 22:04:12 +0530

    updated

--- power_transformation.m
+++ power_transformation.m
@@ -1,7 +1,17 @@
-subplot(1,2,1)
+subplot(3,2,1)
 f = imread('cameraman.tif');
 g = imadjust (f, [0 1], [1 0]);
 imshow(f);
-subplot(1,2,2)
+subplot(3,2,2)
 imshow (g);
+subplot(3,2,3)
+imshow(f);
+subplot(3,2,4)
+g = imadjust (f, [0.5 0.75], [0 1], 0.5);
+imshow(g);
+subplot(3,2,5)
+imshow(f);
+subplot(3,2,6)
+g = imadjust (f, [0.5 0.75], [0.6 1], 0.5);
+imshow(g);
 fprintf('92000103014 Tirth Patel')
